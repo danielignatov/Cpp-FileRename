415493f320bcb22c88f7d06d3aa45ceaa1d4450f
Daniel Ignatov <sa3ana3a@abv.bg>
2016-06-19 16:22:20 +0300

    Notes added

--- FileRename/FileRename/Main.cpp
+++ FileRename/FileRename/Main.cpp
@@ -7,7 +7,7 @@ using namespace boost::filesystem;
 int main()
 {
 	// Current folder path
-	path p{ "C:\\test" }; // TODO: Change on release
+	path p{ "C:\\test" }; // TODO: Change on release - boost::filesystem::system_complete(argv[0]); - you need to remove own program name from it
 
 	// Store all file paths in folder
 	vector<path> folderFilesPaths;
